95f47925f38a4b3b214fa468bc7ab340a0719aef
Sam Deane <sam@elegantchaos.com>
2013-07-14 16:58:43 +0100

    Hooked up unit tests.

--- Source/Tests/ECSecurityTests.m
+++ Source/Tests/ECSecurityTests.m
@@ -1,23 +1,28 @@
 // --------------------------------------------------------------------------
-//! @author Sam Deane
-//! @date 09/12/2011
 //
-//  Copyright 2011 Sam Deane, Elegant Chaos. All rights reserved.
+//  Copyright 2013 Sam Deane, Elegant Chaos. All rights reserved.
 //  This source code is distributed under the terms of Elegant Chaos's 
 //  liberal license: http://www.elegantchaos.com/license/liberal
 // --------------------------------------------------------------------------
 
-#import "ECTestCase.h"
+#import <ECUnitTests/ECUnitTests.h>
 
-
-@interface TestsTests : ECTestCase
+@interface ECSecurityTests : ECTestCase
+{
+}
 
 @end
 
-@implementation TestsTests
+@implementation ECSecurityTests
 
-- (void)test
+- (void)testTest1
 {
+	STAssertTrue(YES, @"dummy test");
+}
+
+- (void)testTest2
+{
+	STAssertTrue(YES, @"dummy test");
 }
 
 @end
